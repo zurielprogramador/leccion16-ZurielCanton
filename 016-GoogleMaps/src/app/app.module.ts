5d9c87b2574fd62baf5c310279357cbb49582ca4
zurielprogramador <argenis.canton08@gmail.com>
2023-11-14 20:06:13 -0500

    video7

--- 016-GoogleMaps/src/app/app.module.ts
+++ 016-GoogleMaps/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
--- 016-GoogleMaps/src/app/material.module.ts
+++ 016-GoogleMaps/src/app/material.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [],
@@ -12,12 +13,14 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatCardModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
